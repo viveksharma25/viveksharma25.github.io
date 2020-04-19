5a8d873192ebabd6af9bee6f7575856bad2f9761
viveksharma25 <62399866+viveksharma25@users.noreply.github.com>
2020-04-19 05:28:38 -0500

    Create myjsfile.js

---
+++ js/myjsfile.js
@@ -0,0 +1,66 @@
+
+$(document).ready(function() {
+    $("#front1").mouseenter(function(){
+        $("#front1").hide();
+        $("#back1").fadeIn("slow");
+       
+    }),
+    $("#back1").mouseleave(function(){
+        $("#back1").hide();
+        $("#front1").fadeIn("slow");
+    }),
+    $("#front2").mouseenter(function(){
+        $("#front2").hide();
+        $("#back2").fadeIn("slow");
+       
+    }),
+    $("#back2").mouseleave(function(){
+        $("#back2").hide();
+        $("#front2").fadeIn("slow");
+    }),
+    $("#front3").mouseenter(function(){
+        $("#front3").hide();
+        $("#back3").fadeIn("slow");
+       
+    }),
+    $("#back3").mouseleave(function(){
+        $("#back3").hide();
+        $("#front3").fadeIn("slow");
+    }),
+    $("#front4").mouseenter(function(){
+        $("#front4").hide();
+        $("#back4").fadeIn("slow");
+       
+    }),
+    $("#back4").mouseleave(function(){
+        $("#back4").hide();
+        $("#front4").fadeIn("slow");
+    }),
+    $("#front5").mouseenter(function(){
+        $("#front5").hide();
+        $("#back5").fadeIn("slow");
+       
+    }),
+    $("#back5").mouseleave(function(){
+        $("#back5").hide();
+        $("#front5").fadeIn("slow");
+    }),
+    $("#front6").mouseenter(function(){
+        $("#front6").hide();
+        $("#back6").fadeIn("slow");
+       
+    }),
+    $("#back6").mouseleave(function(){
+        $("#back6").hide();
+        $("#front6").fadeIn("slow");
+    }),
+    $("#front7").mouseenter(function(){
+        $("#front7").hide();
+        $("#back7").fadeIn("slow");
+       
+    }),
+    $("#back7").mouseleave(function(){
+        $("#back7").hide();
+        $("#front7").fadeIn("slow");
+    })
+});
