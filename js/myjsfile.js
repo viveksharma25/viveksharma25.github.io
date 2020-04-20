e0f59eec28bcb419c8b88d479396cec0f5cadd64
viveksharma25 <62399866+viveksharma25@users.noreply.github.com>
2020-04-19 20:53:23 -0500

    Update myjsfile.js

--- js/myjsfile.js
+++ js/myjsfile.js
@@ -1,65 +1,65 @@
 
 $(document).ready(function() {
-    $("#front1").on("mouseenter touchenter", function(){
+    $("#front1").on("mouseenter touchstart" , function(){
         $("#front1").hide();
         $("#back1").fadeIn("slow");
        
     }),
-    $("#back1").on("mouseleave touchleave",function(){
+    $("#back1").mouseleave(function(){
         $("#back1").hide();
         $("#front1").fadeIn("slow");
     }),
-    $("#front2").on("mouseenter touchenter",function(){
+    $("#front2").on("mouseenter touchstart" , function(){
         $("#front2").hide();
         $("#back2").fadeIn("slow");
        
     }),
-    $("#back2").on("mouseleave touchleave",function(){
+    $("#back2").mouseleave(function(){
         $("#back2").hide();
         $("#front2").fadeIn("slow");
     }),
-    $("#front3").on("mouseenter touchenter",function(){
+    $("#front3").on("mouseenter touchstart" , function(){
         $("#front3").hide();
         $("#back3").fadeIn("slow");
        
     }),
-    $("#back3").on("mouseleave touchleave",function(){
+    $("#back3").mouseleave(function(){
         $("#back3").hide();
         $("#front3").fadeIn("slow");
     }),
-    $("#front4").on("mouseenter touchenter",function(){
+    $("#front4").on("mouseenter touchstart" , function(){
         $("#front4").hide();
         $("#back4").fadeIn("slow");
        
     }),
-    $("#back4").on("mouseleave touchleave",function(){
+    $("#back4").mouseleave(function(){
         $("#back4").hide();
         $("#front4").fadeIn("slow");
     }),
-    $("#front5").on("mouseenter touchenter",function(){
+    $("#front5").on("mouseenter touchstart" , function(){
         $("#front5").hide();
         $("#back5").fadeIn("slow");
        
     }),
-    $("#back5").on("mouseleave touchleave",function(){
+    $("#back5").mouseleave(function(){
         $("#back5").hide();
         $("#front5").fadeIn("slow");
     }),
-    $("#front6").on("mouseenter touchenter",function(){
+    $("#front6").on("mouseenter touchstart" , function(){
         $("#front6").hide();
         $("#back6").fadeIn("slow");
        
     }),
-    $("#back6").on("mouseleave touchleave",function(){
+    $("#back6").mouseleave(function(){
         $("#back6").hide();
         $("#front6").fadeIn("slow");
     }),
-    $("#front7").on("mouseenter touchenter",function(){
+    $("#front7").on("mouseenter touchstart" , function(){
         $("#front7").hide();
         $("#back7").fadeIn("slow");
        
     }),
-    $("#back7").on("mouseleave touchleave",function(){
+    $("#back7").mouseleave(function(){
         $("#back7").hide();
         $("#front7").fadeIn("slow");
     })
