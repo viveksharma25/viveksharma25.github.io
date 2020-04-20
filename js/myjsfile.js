2b7289565121bc3e4eaa43e915d99615f2ed9ca6
viveksharma25 <62399866+viveksharma25@users.noreply.github.com>
2020-04-20 02:03:33 -0500

    Update myjsfile.js

--- js/myjsfile.js
+++ js/myjsfile.js
@@ -1,65 +1,71 @@
 
 $(document).ready(function() {
-    $("#front1").on("mouseenter touchstart" , function(){
+    $("#href1").on("click" , function(){
         $("#front1").hide();
         $("#back1").fadeIn("slow");
        
     }),
-    $("#back1").mouseleave(function(){
-        $("#back1").hide();
-        $("#front1").fadeIn("slow");
-    }),
-    $("#front2").on("mouseenter touchstart" , function(){
+    $("#href2").on("click" , function(){
         $("#front2").hide();
         $("#back2").fadeIn("slow");
        
     }),
-    $("#back2").mouseleave(function(){
-        $("#back2").hide();
-        $("#front2").fadeIn("slow");
-    }),
-    $("#front3").on("mouseenter touchstart" , function(){
+    $("#href3").on("click" , function(){
         $("#front3").hide();
         $("#back3").fadeIn("slow");
        
     }),
-    $("#back3").mouseleave(function(){
-        $("#back3").hide();
-        $("#front3").fadeIn("slow");
-    }),
-    $("#front4").on("mouseenter touchstart" , function(){
+    $("#href4").on("click" , function(){
         $("#front4").hide();
         $("#back4").fadeIn("slow");
        
     }),
-    $("#back4").mouseleave(function(){
-        $("#back4").hide();
-        $("#front4").fadeIn("slow");
-    }),
-    $("#front5").on("mouseenter touchstart" , function(){
+    $("#href5").on("click" , function(){
         $("#front5").hide();
         $("#back5").fadeIn("slow");
        
     }),
-    $("#back5").mouseleave(function(){
-        $("#back5").hide();
-        $("#front5").fadeIn("slow");
-    }),
-    $("#front6").on("mouseenter touchstart" , function(){
+    $("#href6").on("click" , function(){
         $("#front6").hide();
         $("#back6").fadeIn("slow");
        
     }),
-    $("#back6").mouseleave(function(){
-        $("#back6").hide();
-        $("#front6").fadeIn("slow");
-    }),
-    $("#front7").on("mouseenter touchstart" , function(){
+    $("#href7").on("click" , function(){
         $("#front7").hide();
         $("#back7").fadeIn("slow");
        
     }),
-    $("#back7").mouseleave(function(){
+    $("#undo1").click(function(){
+        $("#back1").hide();
+        $("#front1").fadeIn("slow");
+    }),
+    
+    $("#undo2").click(function(){
+        $("#back2").hide();
+        $("#front2").fadeIn("slow");
+    }),
+   
+    $("#undo3").click(function(){
+        $("#back3").hide();
+        $("#front3").fadeIn("slow");
+    }),
+   
+    $("#undo4").click(function(){
+        $("#back4").hide();
+        $("#front4").fadeIn("slow");
+    }),
+    
+    $("#undo5").click(function(){
+        $("#back5").hide();
+        $("#front5").fadeIn("slow");
+    }),
+   
+    $("#undo6").click(function(){
+        $("#back6").hide();
+        $("#front6").fadeIn("slow");
+    }),
+    
+    $("#undo7").click(function(){
         $("#back7").hide();
         $("#front7").fadeIn("slow");
     })
